629173992f3ba36311aa31eea5321592541bd06f
ZORO-1104 <zhangqi_1104@163.com>
2020-06-12 14:06:45 +0800

    迭代器模式，测试3

---
+++ IteratorPattern02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IteratorPattern02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //索引器方式
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
